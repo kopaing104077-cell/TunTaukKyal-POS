2f3bd0ebadc99663f691d1db33751fd5d85e1817
tuntaukkyal <kopaing104077@gmail.com>
2026-09-14 14:08:56 +0630

    Refactor service worker for caching
    Updated cache name and simplified event listeners.

--- sw.js
+++ sw.js
@@ -1,37 +1,4 @@
-const CACHE_NAME = 'ttk-v4';
-
-const ASSETS = [
-  './',
-  './index.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
-});
+const CACHE_NAME = 'ttk-v3';
+const ASSETS = [ './', './index.html' ];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
